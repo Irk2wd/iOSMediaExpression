4d1630afa442878c59089ffddff88d43efb9527a
DragonK <98105716+atom-tracer@users.noreply.github.com>
2026-03-02 15:55:28 +0800

    Initial Commit

---
+++ ImageProcessor/ImageProcessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProcessorApp.swift
+//  ImageProcessor
+//
+//  Created by DragonK on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct ImageProcessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
